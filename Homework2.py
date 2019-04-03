6c1794889aa059c8d46efac762d357dfd71b85b2
Lansing12 <46849105+Lansing12@users.noreply.github.com>
2019-04-03 22:46:08 +0300

    Update Homework2.py

--- Homework2.py
+++ Homework2.py
@@ -1,1 +1,18 @@
+#9.1 Unit converter:
+print ("Hello user, lets convert kilometers into miles:")
+miles = 0.621371
+while True:
+    km = int(input("Please enter kilometers: "))
+    print(km * miles)
+    Yes = input("Do you want to do another conversion? Enter y for YES, n for NO: ")
+    if Yes == "y":
+        km = int(input("Please enter kilometers: "))
+        print(km * miles)
+    elif Yes == "n":
+        break
+print("Thank You and Goodbye!")
+
+#9.3 String lowercase:
+sentence = input("Please enter a sentence: ")
+print(sentence.lower())
 
